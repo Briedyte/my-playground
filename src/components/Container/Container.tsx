54a50c651a66f1e8ec049e75e182d34dd4bfd63e
Emilija Briedyte <em.bredyte@gmail.com>
2022-04-28 20:54:52 +0300

    ContainerVariants added

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,13 +1,30 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { MediaQuery, Spacing } from "@config/style";
 
+export enum ContainerVariant {
+  centeredContent = "centeredContent",
+  sidePaddings = "sidePaddings",
+  allPaddings = "allPaddings",
+}
+
 const ContainerWrapper = styled.div`
-  height: 100%;
-  padding: ${({ withTopBottomMargins }: { withTopBottomMargins: boolean }) =>
-    withTopBottomMargins
-      ? `${Spacing[60]} ${Spacing[100]}`
-      : `0 ${Spacing[100]}`};
+  padding: 0 ${Spacing[100]};
+
+  ${({ variant }: { variant: ContainerVariant }) =>
+    css`
+      ${variant === ContainerVariant.centeredContent &&
+      css`
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      `}
+      ${variant === ContainerVariant.allPaddings &&
+      css`
+        padding: ${Spacing[60]} ${Spacing[100]};
+      `}
+    `}
 
   ${MediaQuery.m} {
     padding: 0 ${Spacing[24]};
@@ -16,18 +33,14 @@ const ContainerWrapper = styled.div`
 
 interface ContainerProps {
   children: React.ReactNode;
-  withTopBottomMargins?: boolean;
+  variant?: ContainerVariant;
 }
 
 const Container = ({
   children,
-  withTopBottomMargins = false,
+  variant = ContainerVariant.allPaddings,
 }: ContainerProps) => {
-  return (
-    <ContainerWrapper withTopBottomMargins={withTopBottomMargins}>
-      {children}
-    </ContainerWrapper>
-  );
+  return <ContainerWrapper variant={variant}>{children}</ContainerWrapper>;
 };
 
 export default Container;
